9c4680bc277f68eb03d7b9898b74b8fde1546d74
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-08-19 15:52:08 +0100

    first test passed journey start

--- models/traveller.js
+++ models/traveller.js
@@ -3,7 +3,10 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const journeysStart = this.journeys.map((journey) => {
+    return journey.startLocation;
+  })
+  return journeysStart;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
